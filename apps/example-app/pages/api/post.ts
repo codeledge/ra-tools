62f26cf6d81367bc77c739c75a771134ba728726
Orlando Groppo <orlando.groppo@gmail.com>
2022-04-22 22:21:55 +0100

    fix create handler (#9)

--- apps/example-app/pages/api/post.ts
+++ apps/example-app/pages/api/post.ts
@@ -3,11 +3,18 @@ import {
   defaultHandler,
   getListHandler,
   GetListRequest,
+  createHandler,
 } from "ra-data-simple-prisma";
 import { prismaClient } from "../../prisma/prismaClient";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.body.method) {
+    case "create":
+      return createHandler(req, res, prismaClient["post"], {
+        connect: {
+          tags: "id",
+        },
+      });
     case "getList":
       return getListHandler(req as GetListRequest, res, prismaClient["post"], {
         include: { tags: true },
