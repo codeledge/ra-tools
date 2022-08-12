dbaae8ef7ce2fdd9821b6995a78c35e0477d3f9d
Orlando Groppo <orlando.groppo@gmail.com>
2022-08-13 01:33:30 +0200

    Add `GetMany` options (#40)
    * Add `GetMany` options
    
    * alphabetise
    
    * getManyReferenceHandler
    
    * upgrade website pkgs
    
    * missing migration

--- packages/ra-data-simple-prisma/src/getManyReferenceHandler.ts
+++ packages/ra-data-simple-prisma/src/getManyReferenceHandler.ts
@@ -3,10 +3,25 @@ import { extractOrderBy } from "./extractOrderBy";
 import { extractSkipTake } from "./extractSkipTake";
 import { extractWhere } from "./extractWhere";
 
-export const getManyReferenceHandler = async (
+export type GetManyRefernceArgs = {
+  include?: object | null;
+  select?: object | null;
+};
+
+export type GetManyReferenceOptions<
+  Args extends GetManyRefernceArgs = GetManyRefernceArgs
+> = {
+  debug?: boolean;
+  include?: Args["include"];
+  select?: Args["select"];
+  transform?: (data: any) => any;
+};
+
+export const getManyReferenceHandler = async <Args extends GetManyRefernceArgs>(
   req: GetManyReferenceRequest,
   res: Response,
-  model: { findMany: Function }
+  model: { findMany: Function },
+  options?: GetManyReferenceOptions<Args>
 ) => {
   const { id, target } = req.body.params;
 
@@ -16,12 +31,18 @@ export const getManyReferenceHandler = async (
 
   const { skip, take } = extractSkipTake(req);
 
-  const list = await model.findMany({
+  // GET DATA
+  const data = await model.findMany({
+    include: options?.include,
+    select: options?.select,
     where: { [target]: id, ...where },
     orderBy,
     skip,
     take,
   });
 
-  res.json({ data: list, total: list.length });
+  // TRANSFORM
+  await options?.transform?.(data);
+
+  res.json({ data, total: data.length });
 };
