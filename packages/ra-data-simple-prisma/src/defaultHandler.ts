89d3d2992860b92f26c59177b5363d8d881f4ffc
nonozgytb <nonozg.ytb@gmail.com>
2022-08-15 13:56:34 +0000

    Add options for GetMany & GetManyRef.
    Add a `getMany` key in the `defaultHandler`'s options.
    It will be used for the `getManyHandler` and `getManyReferenceHandler`.

--- packages/ra-data-simple-prisma/src/defaultHandler.ts
+++ packages/ra-data-simple-prisma/src/defaultHandler.ts
@@ -1,16 +1,16 @@
 import { AuditOptions } from "./audit/types";
 import {
-  CreateRequest,
-  DeleteManyRequest,
-  DeleteRequest,
-  GetListRequest,
-  GetManyReferenceRequest,
-  GetManyRequest,
-  GetOneRequest,
-  Request,
-  Response,
-  UpdateManyRequest,
-  UpdateRequest,
+    CreateRequest,
+    DeleteManyRequest,
+    DeleteRequest,
+    GetListRequest,
+    GetManyReferenceRequest,
+    GetManyRequest,
+    GetOneRequest,
+    Request,
+    Response,
+    UpdateManyRequest,
+    UpdateRequest,
 } from "./Http";
 import { DeleteOptions, deleteHandler } from "./deleteHandler";
 import { GetListOptions, getListHandler } from "./getListHandler";
@@ -18,83 +18,103 @@ import { PrismaClient } from "@prisma/client";
 import { UpdateOptions, updateHandler } from "./updateHandler";
 import { createHandler, CreateOptions } from "./createHandler";
 import { deleteManyHandler } from "./deleteManyHandler";
-import { getManyHandler } from "./getManyHandler";
-import { getManyReferenceHandler } from "./getManyReferenceHandler";
+import { getManyHandler, GetManyOptions } from "./getManyHandler";
+import {
+    getManyReferenceHandler,
+    GetManyReferenceOptions,
+} from "./getManyReferenceHandler";
 import { getOneHandler, GetOneOptions } from "./getOneHandler";
 import { updateManyHandler } from "./updateManyHandler";
 
 export const defaultHandler = async (
-  req: Request,
-  res: Response,
-  prisma: PrismaClient,
-  options?: {
-    create?: CreateOptions;
-    delete?: DeleteOptions;
-    update?: UpdateOptions;
-    getList?: GetListOptions;
-    getOne?: GetOneOptions;
-    audit?: AuditOptions;
-  }
+    req: Request,
+    res: Response,
+    prisma: PrismaClient,
+    options?: {
+        create?: CreateOptions;
+        delete?: DeleteOptions;
+        update?: UpdateOptions;
+        getList?: GetListOptions;
+        getOne?: GetOneOptions;
+        audit?: AuditOptions;
+        getMany?: GetManyOptions;
+    }
 ) => {
-  const modelName = req.body.model || req.body.resource;
-  if (!modelName) throw new Error(`model name is empty`);
+    const modelName = req.body.model || req.body.resource;
+    if (!modelName) throw new Error(`model name is empty`);
 
-  const model = prisma[modelName];
-  if (!model) throw new Error(`No model found for "${modelName}"`);
+    const model = prisma[modelName];
+    if (!model) throw new Error(`No model found for "${modelName}"`);
 
-  switch (req.body.method) {
-    case "getList": {
-      return getListHandler(
-        req as GetListRequest,
-        res,
-        model,
-        options?.getList
-      );
+    switch (req.body.method) {
+        case "getList": {
+            return getListHandler(
+                req as GetListRequest,
+                res,
+                model,
+                options?.getList
+            );
+        }
+        case "getOne": {
+            return getOneHandler(
+                req as GetOneRequest,
+                res,
+                model,
+                options?.getOne
+            );
+        }
+        case "getMany": {
+            return getManyHandler(
+                req as GetManyRequest,
+                res,
+                model,
+                options?.getMany
+            );
+        }
+        case "getManyReference": {
+            return getManyReferenceHandler(
+                req as GetManyReferenceRequest,
+                res,
+                model,
+                options?.getMany
+            );
+        }
+        case "create": {
+            return await createHandler(req as CreateRequest, res, model, {
+                ...options?.create,
+                audit: options?.audit,
+            });
+        }
+        case "update": {
+            return await updateHandler(req as UpdateRequest, res, model, {
+                ...options?.update,
+                audit: options?.audit,
+            });
+        }
+        case "updateMany": {
+            return await updateManyHandler(
+                req as UpdateManyRequest,
+                res,
+                model,
+                {
+                    ...options?.update,
+                    audit: options?.audit,
+                }
+            );
+        }
+        case "delete": {
+            return await deleteHandler(req as DeleteRequest, res, model, {
+                ...options?.delete,
+                audit: options?.audit,
+            });
+        }
+        case "deleteMany": {
+            return deleteManyHandler(req as DeleteManyRequest, res, model, {
+                ...options?.delete,
+                audit: options?.audit,
+            });
+        }
+        default:
+            throw new Error("Invalid method");
     }
-    case "getOne": {
-      return getOneHandler(req as GetOneRequest, res, model, options?.getOne);
-    }
-    case "getMany": {
-      return getManyHandler(req as GetManyRequest, res, model);
-    }
-    case "getManyReference": {
-      return getManyReferenceHandler(
-        req as GetManyReferenceRequest,
-        res,
-        model
-      );
-    }
-    case "create": {
-      return await createHandler(req as CreateRequest, res, model, {
-        ...options?.create,
-        audit: options?.audit,
-      });
-    }
-    case "update": {
-      return await updateHandler(req as UpdateRequest, res, model, {
-        ...options?.update,
-        audit: options?.audit,
-      });
-    }
-    case "updateMany": {
-      return await updateManyHandler(req as UpdateManyRequest, res, model, {
-        ...options?.update,
-        audit: options?.audit,
-      });
-    }
-    case "delete": {
-      return await deleteHandler(req as DeleteRequest, res, model, {
-        ...options?.delete,
-        audit: options?.audit,
-      });
-    }
-    case "deleteMany": {
-      return deleteManyHandler(req as DeleteManyRequest, res, model, {
-        ...options?.delete,
-        audit: options?.audit,
-      });
-    }
-    default:
-      throw new Error("Invalid method");
-  }
 };
