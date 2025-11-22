2adba58e46050736593235bea4d24ef15013fef5
Entitree <entitree.app@gmail.com>
2025-11-22 19:42:48 +0800

    init prisma v7

---
+++ packages/db/prisma.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    seed: "tsx prisma/seed.ts",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
